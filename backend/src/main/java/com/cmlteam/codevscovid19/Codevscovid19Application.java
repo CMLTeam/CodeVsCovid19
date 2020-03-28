9d6856bca7334dcbe0030725d1b5891f24a1b51f
korzin <Samkorzin@gmail.com>
2020-03-28 15:09:55 +0200

    backend boot template

---
+++ backend/src/main/java/com/cmlteam/codevscovid19/Codevscovid19Application.java
@@ -0,0 +1,13 @@
+package com.cmlteam.codevscovid19;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Codevscovid19Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Codevscovid19Application.class, args);
+	}
+
+}
