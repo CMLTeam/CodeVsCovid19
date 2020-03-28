2295326d0732768e19f3ebe36cb2f73580b079ea
G0yter <goyter012@gmail.com>
2020-03-28 21:20:14 +0200

    bottom bar added

---
+++ mobile/Covidapp/app/src/main/java/fragments/ProfileFragment.kt
@@ -0,0 +1,29 @@
+package fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import com.example.covid_app.R
+
+class ProfileFragment: Fragment(){
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_profile, container, false)
+        view.findViewById<TextView>(R.id.textView_test).text = "Profile is here"
+
+
+        return view
+    }
+
+
+    companion object{
+        fun newInstance(): ProfileFragment = ProfileFragment()
+    }
+}
