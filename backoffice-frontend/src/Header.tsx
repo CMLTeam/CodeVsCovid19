00087dda65d4a59289bec8ff761335f7d0c921a8
Kopylov Oleksii <tonn.tamerlan@gmail.com>
2020-03-29 22:01:17 +0300

    header

--- backoffice-frontend/src/Header.tsx
+++ backoffice-frontend/src/Header.tsx
@@ -1,15 +1,28 @@
-import React from 'react';
+import React from "react";
 import logo from "./COVID-19_World_Map.svg";
 
-interface HeaderProps {
-
-}
+interface HeaderProps {}
 
 export const Header = (props: HeaderProps) => {
   return (
     <div>
-      <img style={{maxHeight: "10vh", width: "80vw"}} src={logo} alt="logo" />
+      <div style={{ display: "inline-block" }}>
+        <img
+          style={{ height: "9vw", maxWidth: "50vh" }}
+          src={logo}
+          alt="logo"
+        />
+      </div>
 
+      <div
+        style={{
+          display: "inline-block",
+          fontSize: "11vw",
+          width: "70%",
+        }}
+      >
+        C O V o I D e r
+      </div>
     </div>
   );
 };
