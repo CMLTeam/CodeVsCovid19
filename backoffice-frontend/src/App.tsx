1cd014c5459c93c67f5a4a6d61567498d38242c6
Kopylov Oleksii <tonn.tamerlan@gmail.com>
2020-03-29 12:52:26 +0300

    axios and prettier

--- backoffice-frontend/src/App.tsx
+++ backoffice-frontend/src/App.tsx
@@ -1,18 +1,16 @@
-import React from 'react';
-import logo from './COVID-19_World_Map.svg';
-import './App.css';
+import React from "react";
+import logo from "./COVID-19_World_Map.svg";
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Update Cool frontend for reduce covid spreading.
-        </p>
+        <p>Update Cool frontend for reduce covid spreading.</p>
       </header>
     </div>
   );
-}
+};
 
 export default App;
