ea6402454226a2d484269f7221041c158f8ad5f7
Kopylov Oleksii <tonn.tamerlan@gmail.com>
2020-03-29 12:23:10 +0300

    deploy script

---
+++ backoffice-frontend/deploy_front.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -ex;
+
+# remove old resources
+rm -rf ./build/
+ssh tamerlan@cmlteam.com 'rm -rf covid_backoffice/build/*'
+
+# build and copy resources
+npm run-script build
+scp -r ./build/ tamerlan@cmlteam.com:./covid_backoffice
+
+# start frontend
+ssh tamerlan@cmlteam.com '
+
+PORT=$(sudo lsof -t -i:8098);
+
+if [[ $PORT ]]
+then
+sudo kill -9 $PORT;
+fi;
+
+serve -s -n -l 8098 ./covid_backoffice/build/ &>/dev/null &
+
+' &
--- backoffice-frontend/src/App.tsx
+++ backoffice-frontend/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Cool frontend for reduce covid spreading.
+          Update Cool frontend for reduce covid spreading.
         </p>
       </header>
     </div>
