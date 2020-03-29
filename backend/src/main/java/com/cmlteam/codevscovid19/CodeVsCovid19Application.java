d19e85983236789dbd1a28c9d838c0e073a3d723
korzin <Samkorzin@gmail.com>
2020-03-29 23:19:07 +0300

    backend - PopulateService

--- backend/src/main/java/com/cmlteam/codevscovid19/CodeVsCovid19Application.java
+++ backend/src/main/java/com/cmlteam/codevscovid19/CodeVsCovid19Application.java
@@ -2,12 +2,15 @@ package com.cmlteam.codevscovid19;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class CodeVsCovid19Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(CodeVsCovid19Application.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(CodeVsCovid19Application.class, args);
+
+        context.getBean(PopulateService.class).populateTables();
     }
 
 }
