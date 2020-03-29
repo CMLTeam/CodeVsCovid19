932f110bcc8d49510fc9b48ee4393f01bd367806
korzin <Samkorzin@gmail.com>
2020-03-29 22:59:06 +0300

    backend - models - allargconstructor

--- backend/src/main/java/com/cmlteam/codevscovid19/models/Target.java
+++ backend/src/main/java/com/cmlteam/codevscovid19/models/Target.java
@@ -1,11 +1,13 @@
 package com.cmlteam.codevscovid19.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Data
+@AllArgsConstructor
 public class Target {
 
     private Integer id;
